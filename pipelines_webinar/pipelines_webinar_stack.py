c29d0a8537c13a41d93a4f95b9af519859b9ac40
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2020-09-08 14:22:04 -0500

    Add files via upload

---
+++ pipelines_webinar/pipelines_webinar_stack.py
@@ -0,0 +1,23 @@
+from os import path
+
+from aws_cdk import core
+import aws_cdk.aws_lambda as lmb
+import aws_cdk.aws_apigateway as apigw
+
+class PipelinesWebinarStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        this_dir = path.dirname(__file__)
+        handler = lmb.Function(self,'handler', 
+                runtime=lmb.Runtime.PYTHON_3_7, 
+                handler='handler.handler', 
+                code=lmb.Code.from_asset(path.join(this_dir, 'lambda')))
+        
+        gw = apigw.LambdaRestApi(self, 'Gateway', 
+                description='Endpoint for a simple lambda',
+                handler = handler.current_version)
+
+        self.url_output = core.CfnOutput(self,'Url',
+                value=gw.url)  
